248c2697976f31883f6c9f24bf9623aa6503d8d4
hyranno <hyranno4pub@gmail.com>
2023-06-13 09:56:21 +0900

    emit human-readable hlsl

--- shader/build.rs
+++ shader/build.rs
@@ -1,9 +1,10 @@
-use spirv_builder::{MetadataPrintout, SpirvBuilder};
+use spirv_builder::{MetadataPrintout, SpirvBuilder, SpirvMetadata};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let shader_crate = "shader_src";
     let target = "spirv-unknown-spv1.5";
     SpirvBuilder::new(shader_crate, target)
+        .spirv_metadata(SpirvMetadata::Full)
         .print_metadata(MetadataPrintout::Full)
         .build()?;
 
